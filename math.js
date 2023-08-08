a134e679132e1c1e603538082a788b90f8d4aa19
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-08-08 15:27:17 -0400

    Update math.js

--- math.js
+++ math.js
@@ -1,6 +1,7 @@
-const add = (a,b) => a + b;
-const subtract = (a,b) => a - b;
-const multiply =  (a,b) => a * b;
-const divide = (a,b) => a / b;
+exports.add = (a,b) => a + b;
+exports.subtract = (a,b) => a - b;
+exports.multiply =  (a,b) => a * b;
+exports.divide = (a,b) => a / b;
 
-module.exports = {add, subtract, multiply, divide}
+// alternately exports could be handled this way: 
+// module.exports = {add, subtract, multiply, divide}
