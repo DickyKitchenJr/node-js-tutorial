99f42d14029f53e5f5d6c1336aa2a4db8bd03263
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-08-08 15:27:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,19 +7,26 @@ console.log("Yep, I'm working");
 // console.log(global);
 
 // 4. uses commonjs modules vs ES6 modules (require vs import)
+// 5. node.js is missing some javascript APIs like fetch
+
 const os = require('os');
 const path = require('path');
-const math = require('./math');
+const { add, subtract, multiply, divide } = require('./math');
+
+console.log(add(2,2));
+console.log(subtract(5,2));
+console.log(multiply(1,2));
+console.log(divide(2,2));
 
 
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
 
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
-console.log(path.parse(__filename));
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+// console.log(path.parse(__filename));
