0e5de3fe917b64fa66a303bc640b09c30efa494b
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-08-08 17:10:30 -0400

    moved files
    moved files into properly named folder

--- math.js
+++
@@ -1,7 +0,0 @@
-exports.add = (a,b) => a + b;
-exports.subtract = (a,b) => a - b;
-exports.multiply =  (a,b) => a * b;
-exports.divide = (a,b) => a / b;
-
-// alternately exports could be handled this way: 
-// module.exports = {add, subtract, multiply, divide}
