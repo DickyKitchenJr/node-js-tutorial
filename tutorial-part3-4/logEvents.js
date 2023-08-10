99c8e3f647ffa79c178737c5094ffef0c93b69e9
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-08-10 16:28:37 -0400

    results from event emitter
    part 4 went over building an event emitter

---
+++ tutorial-part3-4/logEvents.js
@@ -0,0 +1,31 @@
+// npm modules
+
+const { format } = require("date-fns");
+// using the v4 : uuid is like saying import as, so in this case it's importing version 4 as the variable uuid
+const { v4: uuid } = require("uuid");
+
+// common core modules
+
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  console.log(logItem);
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", "eventLog.txt"),
+      logItem
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = logEvents;
