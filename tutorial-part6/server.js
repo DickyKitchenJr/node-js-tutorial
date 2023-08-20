5903c38d4f2861c65543ac52c034b71f08d35c46
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-08-20 16:29:37 -0400

    Update server.js
    finished part 6 of the tutorial

--- tutorial-part6/server.js
+++ tutorial-part6/server.js
@@ -1,99 +1,56 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter { };
-// initialize object 
-const myEmitter = new Emitter();
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+const express = require("express");
+const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        const rawData = await fsPromises.readFile(
-            filePath,
-            !contentType.includes('image') ? 'utf8' : ''
-        );
-        const data = contentType === 'application/json'
-            ? JSON.parse(rawData) : rawData;
-        response.writeHead(
-            filePath.includes('404.html') ? 404 : 200,
-            { 'Content-Type': contentType }
-        );
-        response.end(
-            contentType === 'application/json' ? JSON.stringify(data) : data
-        );
-    } catch (err) {
-        console.log(err);
-        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
-        response.statusCode = 500;
-        response.end();
-    }
+// express handles routes from top to bottom, so you want to start with your root route '/' then build from there
+
+// '^/$|index.html' translates to the request will begin with a slash ('^/'), end with a slash ('/$'), or end in index ('|index') with .html optional ('(.html)?')
+// so combined it is '^/$|index(.html)?'
+app.get("^/$|index(.html)?", (req, res) => {
+  // res.sendFile('./views/index.html', {root: __dirname});
+  // does the same as below
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
+
+// chaining rountes with route handlers
+// first way
+app.get('/hello(.html)?', (req, res, next) =>{
+    console.log('here is the first attempted thing');
+    next();
+}, (req, res) =>{
+    res.send('Here is the second thing');
+})
+
+// second way
+const one = (req, res, next) =>{
+    console.log('one');
+    next();
 }
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+const two = (req, res, next) => {
+  console.log("two");
+  next();
+};
 
-    const extension = path.extname(req.url);
+const three = (req, res) =>{
+    console.log("three");
+    res.send('Finished');
+}
 
-    let contentType;
+app.get('/chained(.html)?', [one, two, three]);
 
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.txt':
-            contentType = 'text/plain';
-            break;
-        default:
-            contentType = 'text/html';
-    }
-
-    let filePath =
-        contentType === 'text/html' && req.url === '/'
-            ? path.join(__dirname, 'views', 'index.html')
-            : contentType === 'text/html' && req.url.slice(-1) === '/'
-                ? path.join(__dirname, 'views', req.url, 'index.html')
-                : contentType === 'text/html'
-                    ? path.join(__dirname, 'views', req.url)
-                    : path.join(__dirname, req.url);
-
-    // makes .html extension not required in the browser
-    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
-
-    const fileExists = fs.existsSync(filePath);
-
-    if (fileExists) {
-        serveFile(filePath, contentType, res);
-    } else {
-        switch (path.parse(filePath).base) {
-            case 'old-page.html':
-                res.writeHead(301, { 'Location': '/new-page.html' });
-                res.end();
-                break;
-            case 'www-page.html':
-                res.writeHead(301, { 'Location': '/' });
-                res.end();
-                break;
-            default:
-                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-        }
-    }
+// because of the top down way that express runs, it's good to end your routes with a catch-all for when non-existing routes are input by the user
+app.get("/*", (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
